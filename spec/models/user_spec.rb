a6f9a1f1e0327354159f057eaa33b8898c7fedd2
jimasan01 <takayuki01200216@gmail.com>
2023-01-23 19:33:09 +0900

    バリデーション修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,6 +49,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password Please include both letters and numbers')
       end
+      
       it "passwordが5文字以下では登録できない" do
         @user.password = '12345'
         @user.password_confirmation = '12345'
