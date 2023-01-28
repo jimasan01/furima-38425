531f1e6a5150eb80f415ed557aa93aee39de7d20
jimasan01 <takayuki01200216@gmail.com>
2023-01-28 20:45:51 +0900

    テスト項目追加、修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -40,7 +40,7 @@ end
       it 'ユーザー登録している人でないと出品できない' do
         @item.user_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist', "User can't be blank")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
       it '画像がないと出品できない' do
         @item.image = nil
@@ -65,7 +65,7 @@ end
       it 'カテゴリーの情報が空欄だと出品できない' do
         @item.category_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank", 'Category is not a number')
+        expect(@item.errors.full_messages).to include('Category is not a number')
       end
       it '商品の状態の情報が「---」だと出品できない' do
         @item.state_id = 1
@@ -75,7 +75,7 @@ end
       it '商品の状態の情報が空欄だと出品できない' do
         @item.state_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("State can't be blank", 'State is not a number')
+        expect(@item.errors.full_messages).to include('State is not a number')
       end
       it '配送料の負担の情報が「---」だと出品できない' do
         @item.postage_id = 1
@@ -85,7 +85,7 @@ end
       it '配送料の負担の情報が空欄だと出品できない' do
         @item.postage_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Postage can't be blank", 'Postage is not a number')
+        expect(@item.errors.full_messages).to include('Postage is not a number')
       end
       it '発送元の地域の情報が「---」だと出品できない' do
         @item.area_id = 1
@@ -95,7 +95,7 @@ end
       it '発送元の地域の情報が空欄だと出品できない' do
       @item.area_id = nil
       @item.valid?
-      expect(@item.errors.full_messages).to include("Area can't be blank", 'Area is not a number')
+      expect(@item.errors.full_messages).to include('Area is not a number')
       end
       it '発送までの日数の情報が「---」だと出品できない' do
         @item.sipping_day_id = 1
@@ -105,12 +105,17 @@ end
       it '発送までの日数の情報が空欄だと出品できない' do
         @item.sipping_day_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Sipping day can't be blank", 'Sipping day is not a number')
+        expect(@item.errors.full_messages).to include('Sipping day is not a number')
       end
       it '価格が空欄だと出品できない' do
         @item.price = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number')
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格に全角が含まれると出品できない' do
+        @item.price = 'あああ'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it '価格の範囲が、300円未満だと出品できない' do
         @item.price = 100
