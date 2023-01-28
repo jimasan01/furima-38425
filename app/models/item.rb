807e696d628588107cbc20d61ae9082a27a5216d
jimasan01 <takayuki01200216@gmail.com>
2023-01-28 20:45:25 +0900

    修正(user_id不要、activehash の項目にpresence: true不要)

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,15 +13,9 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :user_id
     validates :name
     validates :image
     validates :explanation
-    validates :category_id
-    validates :state_id
-    validates :postage_id
-    validates :area_id
-    validates :sipping_day_id
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
@@ -32,5 +26,4 @@ class Item < ApplicationRecord
     validates :area_id
     validates :sipping_day_id
   end
-
 end
