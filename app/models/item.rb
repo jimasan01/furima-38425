b3e0aba856901d20cb47a2cda151d6d69578ccae
jimasan01 <takayuki01200216@gmail.com>
2023-01-28 15:27:55 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,36 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  #ActiveHashのアソシエーション
+  belongs_to :category
+  belongs_to :state
+  belongs_to :postage
+  belongs_to :area
+  belongs_to :sipping_day
+
   belongs_to :user
+
+  has_one_attached :image
 
   with_options presence: true do
     validates :user_id
+    validates :name
+    validates :image
     validates :explanation
     validates :category_id
     validates :state_id
     validates :postage_id
     validates :area_id
     validates :sipping_day_id
-    validates :price
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :postage_id
+    validates :area_id
+    validates :sipping_day_id
+  end
+
 end
